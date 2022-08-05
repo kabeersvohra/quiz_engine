992a911b71901ae51e27d6c1fc2f16372ed97357
Kabeer Vohra <kabeer@pc>
2022-08-05 18:03:06 +0200

    Remove query file

--- query.py
+++
@@ -1,8 +0,0 @@
-import graphene
-
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(description="A typical hello world")
-
-    def resolve_hello(self, info):
-        return "World"
